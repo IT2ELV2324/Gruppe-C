9eaf30f1be4b40adfcc4e090610371298072d872
sondreaa <104071423+sondreaa@users.noreply.github.com>
2023-09-27 11:04:23 +0200

    dialog-funksjon

--- eventyrspill/spill.py
+++ eventyrspill/spill.py
@@ -1,3 +1,15 @@
 """Hovedfilen"""
 import items
 import karakterer
+
+dialog = ['HER LIGGER ALL DIALOG']
+
+def load_dialog():
+    dialog_file = open("./eventyrspill/dialog.txt", encoding="utf-8")
+
+    for line in dialog_file:  
+        dialog.append(line.strip("\n"))
+
+load_dialog()
+print(dialog[1])
+#hent dialog ved å bruke dialog[i], der i er linjen i dialog.txt hvor teksten ligger.
